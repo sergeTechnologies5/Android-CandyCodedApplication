a8631e069030473ca47147d1da34c5d8f8d8f317
= <njesh74@gmail.com>
2019-11-08 15:08:57 +0300

    Added Showing Information Activity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -77,8 +78,18 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
+
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent inforActivityIntent = new Intent(this, InfoActivity.class);
+        startActivity(inforActivityIntent);
+        return super.onOptionsItemSelected(item);
+    }
+
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
